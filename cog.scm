f736351aecb0c1c3728679d82aa5cd9c5c548249
Thomas Schafer <thomasschafer97@gmail.com>
2025-07-03 20:26:02 +0100

    Bump version to 0.1.1

--- cog.scm
+++ cog.scm
@@ -1,5 +1,5 @@
 (define package-name 'scooter)
-(define version "0.1.0")
+(define version "0.1.1")
 
 (define dependencies '())
 (define dylibs
@@ -7,16 +7,16 @@
      #:urls
      ((#:platform "x86_64-windows"
        #:url
-       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.0/libscooter_hx.x86_64-windows.dll")
+       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.1/libscooter_hx.x86_64-windows.dll")
       (#:platform "x86_64-macos"
        #:url
-       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.0/libscooter_hx.aarch64-macos.dylib")
+       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.1/libscooter_hx.aarch64-macos.dylib")
       (#:platform "aarch_64-macos"
        #:url
-       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.0/libscooter_hx.aarch64-macos.dylib")
+       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.1/libscooter_hx.aarch64-macos.dylib")
       (#:platform "x86_64-linux"
        #:url
-       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.0/libscooter_hx.x86_64-linux.so")
+       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.1/libscooter_hx.x86_64-linux.so")
       (#:platform "aarch64-linux"
        #:url
-       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.0/libscooter_hx.aarch64-linux.so")))))
+       "https://github.com/thomasschafer/scooter.hx/releases/download/v0.1.1/libscooter_hx.aarch64-linux.so")))))
