83f36fc49fa9697c7290922fa6a5466be35413a1
Thomas Schafer <thomasschafer97@gmail.com>
2025-08-24 15:18:04 +0100

    Fix linting errors

--- src/logging.rs
+++ src/logging.rs
@@ -16,10 +16,10 @@ pub fn default_log_file() -> PathBuf {
 }
 
 fn make_parent_dir(path: &Path) -> std::io::Result<()> {
-    if let Some(parent) = path.parent() {
-        if !parent.exists() {
-            std::fs::create_dir_all(parent)?;
-        }
+    if let Some(parent) = path.parent()
+        && !parent.exists()
+    {
+        std::fs::create_dir_all(parent)?;
     }
     Ok(())
 }
